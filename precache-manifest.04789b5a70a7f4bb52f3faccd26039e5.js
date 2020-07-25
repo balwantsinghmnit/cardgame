22e60f4886b79cfec6a71f395e4da33b52085693
balwantsinghmnit <balwantsinghmnit@gmail.com>
2020-07-25 20:22:46 +0530

    Updates

---
+++ precache-manifest.04789b5a70a7f4bb52f3faccd26039e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cba13409d3809da06c1fc516a1208ab",
+    "url": "/cardgame/index.html"
+  },
+  {
+    "revision": "06456478f21d0d6b2ee4",
+    "url": "/cardgame/static/css/main.bb7f868f.chunk.css"
+  },
+  {
+    "revision": "e520a6220585c72530fe",
+    "url": "/cardgame/static/js/2.a51db400.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cardgame/static/js/2.a51db400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06456478f21d0d6b2ee4",
+    "url": "/cardgame/static/js/main.6b6c9f78.chunk.js"
+  },
+  {
+    "revision": "4ba5c00a78d22e562741",
+    "url": "/cardgame/static/js/runtime-main.85c7a9f0.js"
+  }
+]);
